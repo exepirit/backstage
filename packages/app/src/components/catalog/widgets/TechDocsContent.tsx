bc9332d206a86daf95a78f4bb79dd16531f9407f
exepirit <exepir1t@protonmail.com>
2024-08-06 00:07:44 +0700

    feat: mermaid diagrams plugin

--- packages/app/src/components/catalog/widgets/TechDocsContent.tsx
+++ packages/app/src/components/catalog/widgets/TechDocsContent.tsx
@@ -1,12 +1,14 @@
 import { EntityTechdocsContent } from '@backstage/plugin-techdocs';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
+import { Mermaid } from 'backstage-plugin-techdocs-addon-mermaid';
 import React from 'react';
 
 export const techDocsContent = (
   <EntityTechdocsContent>
     <TechDocsAddons>
       <ReportIssue />
+      <Mermaid />
     </TechDocsAddons>
   </EntityTechdocsContent>
 );
