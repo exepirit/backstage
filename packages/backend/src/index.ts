5a2099952834e967308553fbe44fec51a3dd3cf5
exepirit <exepir1t@protonmail.com>
2024-09-17 21:34:36 +0700

    feat(backend): notifications

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -46,4 +46,7 @@ backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
+// notifications
+backend.add(import('@backstage/plugin-notifications-backend'));
+
 backend.start();
